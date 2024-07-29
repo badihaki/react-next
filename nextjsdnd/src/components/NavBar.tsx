5fc5b171ddac38eb5388af8c49c17594233cd06f
B. Haki Martin <jojackblack@gmail.com>
2024-07-29 10:29:18 -0400

    initialized redux, providing the redux store to the entire app

--- nextjsdnd/src/components/NavBar.tsx
+++ nextjsdnd/src/components/NavBar.tsx
@@ -1,10 +1,10 @@
 import NavBarProps from "@/interfaces/INavBarProps";
 import Link from "next/link";
 
-export default function NavigationBar(links:NavBarProps[]){
-    console.log(links.links);
+export default function NavigationBar({links}: {links:NavBarProps[]}){
+    // console.log(links.links);
 
-    const navLinks = links.links.map( (link: {
+    const navLinks = links.map( (link: {
         title: string,
         url: string
     }) => <div key={link.title+link.url}>
