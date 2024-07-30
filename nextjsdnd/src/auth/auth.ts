b5ef65d005577f0dbb6453e3cf37cafe25d46f30
B. Haki Martin <jojackblack@gmail.com>
2024-07-30 13:22:49 -0400

    added mongoose, made user model, auth script and api path with boilerplate script

---
+++ nextjsdnd/src/auth/auth.ts
@@ -0,0 +1,39 @@
+import { connectDB } from "@/mongo/mongodb";
+import User from "@/models/User";
+import type { NextAuthOptions } from "next-auth";
+import credentials from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import { emit } from "process";
+import Email from "next-auth/providers/email";
+
+export const authOptions:NextAuthOptions = {
+    providers:[
+        credentials({
+            name: "Credentials",
+            id: "credentials",
+            credentials: {
+              email: { label: "Email", type: "text" },
+              password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials){
+                await connectDB();
+                const user = await User.findOne({
+                email: credentials?.email,
+                }).select("+password");
+
+                if (!user) throw new Error("Wrong Email");
+
+                const passwordMatch = await bcrypt.compare(
+                credentials!.password,
+                user.password
+                );
+
+                if (!passwordMatch) throw new Error("Wrong Password");
+                return user;
+            }
+        }),
+    ],
+    session:{
+        strategy: "jwt",
+    }
+};
